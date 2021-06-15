3251c7ef08e3735abff0bc6573b91ca4dd0f8a8c
surendra916 <56813758+surendra916@users.noreply.github.com>
2021-06-15 16:56:02 +0530

    added port

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('haiii world');
 })
- 
 
-app.listen(4000,console.log('server running on the port 4000'))
+const PORT = process.env.port || 4000;
+
+app.listen(PORT,console.log('server running on the port 4000'))
